af68b9c21ea148d6087eac0314326fcf08615afa
SAGAR RUDAGI <sagarrudagi@gmail.com>
2019-08-07 15:44:57 +0530

    Update lab0Add.py

--- lab0Add.py
+++ lab0Add.py
@@ -1,6 +1,14 @@
 """This python file is associated with lab0Add.txt
 The following terminal command is used to execute it
 python3 lab0Add.py<lab0Add.txt"""
+
+"""The lab0Add.txt file contains the following content :
+2
+5 6
+1 2 3 4 6
+5 2
+1 3 4 5 6"""
+
 tc = int(input())
 for i in range(tc):
 	arr = []
