385a91b0287928b972cac7d6dc1d128ab4a52e03
SAGAR RUDAGI <sagarrudagi@gmail.com>
2019-09-04 12:57:21 +0530

    Add files via upload

---
+++ lab4_DFS.py
@@ -0,0 +1,28 @@
+n = int(input())
+adjacent=[]
+for i in range(n):
+	adjacent.append(list(map(int,input().split())))
+
+
+visited=[]
+for i in range(n):
+	visited.append(0)
+
+
+def DFS(v):
+	print(v," ",end="")
+	visited[v]=1
+	for i in range(n):
+		#print(i)
+		if(adjacent[v][i]==1 and visited[i]==0):
+			DFS(i)
+
+
+for i in range(n):
+	if(visited[i]==0):
+		#print(i," ", end="")
+		DFS(i)
+		print()
+
+
+
