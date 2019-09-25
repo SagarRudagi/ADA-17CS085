b3d3bb7d5e793c036345c0c643722979dde4dff6
SAGAR RUDAGI <sagarrudagi@gmail.com>
2019-09-25 12:35:06 +0530

    Update lab5_bfs.py

--- lab5_bfs.py
+++ lab5_bfs.py
@@ -14,13 +14,19 @@ class graph:
         print(v)
         while(len(q)!=0):
             w=q.pop(0)
+            #print(w)
             for i in range(self.c):
-                if self.g[v][i]==1 and visited[i]==False:
+                if self.g[w][i]==1 and visited[i]==False:
                     visited[i]=True
                     q.append(i)
                     print(i)
-                    q.pop(0)
+                    #q.pop(0)
+
                 
-g=[[1,1,1,0],[1,0,0,0],[1,1,1,0],[0,0,0,1]]
-m=graph(g,4,4)
+n=int(input())
+g=[]
+for i in range(n):
+    g.append(list(map(int,input().split())))
+
+m = graph(g,n,n)
 m.bfs(2)
