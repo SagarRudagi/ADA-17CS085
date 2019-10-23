0e0a3a7cf2ef067a7c543f8ec3392af1f3052551
SAGAR RUDAGI <sagarrudagi@gmail.com>
2019-10-23 12:49:42 +0530

    Update and rename smallestRange.py to lab8_2_smallestRange.py

--- lab8_2_smallestRange.py
+++ lab8_2_smallestRange.py
@@ -1,22 +1,22 @@
 import math
 k = int(input())
+arr = []
 for i in range(k):
     eleList = list(map(int,input().split()))
-    arr = []
     arr.append(eleList)
 ptr = [0]*k
 n = len(arr[0])
 
 def findSmallestRange(arr, n, k):  
-    i, minval, maxval, minrange, minel, maxel, flag, minind = 0, -math.inf, math.inf, math.inf + math.inf, 0, 0, 0, 0 
+    i, minrange, minel, maxel, flag, minind = 0, math.inf, 0, 0, 0, 0 
+
     while(1):     
         minind = -1
-        # minval = 10**9
-        # maxval = -10**9
+        minval = 10**9
+        maxval = -10**9
         flag = 0
-  
-        for i in range(k): 
 
+        for i in range(k): 
             if(ptr[i] == n): 
                 flag = 1    
                 break
@@ -38,4 +38,6 @@ def findSmallestRange(arr, n, k):
             maxel = maxval 
             minrange = maxel - minel 
       
-    print("The smallest range is [", minel, maxel,"]") 
+    print([minel, maxel]) 
+
+findSmallestRange(arr,n,k)
