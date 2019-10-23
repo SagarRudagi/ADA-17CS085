aa26cf617af8b73cb1c195b5e4bdd2feccc5d8cc
SAGAR RUDAGI <sagarrudagi@gmail.com>
2019-10-23 11:29:39 +0530

    Update knapsack.py

--- knapsack.py
+++ knapsack.py
@@ -1,26 +1,16 @@
-n = int(input()) #No of items
-wv_val = []
-for i in range(n):
-	wt = list(map(int,input().split()))
-	wv_val.append(wt)
-
-# print(wv_val)
-wv_val.sort()
-# print(wv_val)
-# wt_val = sorted(wt_val)
-
-limit_wt = int(input()) #Knapsack Limit
-
-table = [[0]*(limit_wt+1) for i in range(n)]
-# print(table)
-
-for i in range(n):
-	for j in range(limit_wt+1):
-		if wv_val[i][1]<=j:
-			table[i][j] = max(table[i-1][j],(wv_val[i][1]+table[i-1][j-wv_val[i][0]]))
-		elif wv_val[i][1]>j:
-			table[i][j] = table[i-1][j]
-
-
-for i in range(len(table)):
-	print(table[i])
+def knapSack(W, wt, val, n): 
+    K = [[0 for x in range(W+1)] for x in range(n+1)] 
+    for i in range(n+1): 
+        for w in range(W+1): 
+            if i==0 or w==0: 
+                K[i][w] = 0
+            elif wt[i-1] <= w: 
+                K[i][w] = max(val[i-1] + K[i-1][w-wt[i-1]],  K[i-1][w]) 
+            else: 
+                K[i][w] = K[i-1][w]
+    return K[n][W] 
+val = list(map(int,input("Enter the values of items").split()))
+wt = list(map(int,input("Enter the weights of items").split()))
+W = int(input("Enter the max weight"))
+n = len(val) 
+print(knapSack(W, wt, val, n)) 
